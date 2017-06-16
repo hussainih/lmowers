548c30266a29291809cf9b6b297eaee8ca455e26
Hussaini <hussaini.mohd86@gmail.com>
2017-06-16 18:49:45 +0200

    committing for google cloud services changes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   for(  a=0; a<=10000000; a++){
-    for(b=0;b<=1000000; b++){
+    for(b=0;b<=100; b++){
       c =a*b;
     }
 
